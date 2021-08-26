0712172c17a41966012cd031c891542bf39196b7
LukaszSztajerowski <lukaszsztajerowski@gmail.com>
2021-08-26 20:36:54 +0200

    show list

---
+++ src/main/java/pl/coderslab/BookController.java
@@ -0,0 +1,24 @@
+package pl.coderslab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+
+    @RequestMapping("/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+    @GetMapping("")
+    public List<Book> showBooks(MockBookService mockBookService){
+        return mockBookService.getList();
+    }
+}
